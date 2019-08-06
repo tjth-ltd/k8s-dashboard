7779c0ce3ad302034e236667ff4326d35dfa9f6d
TJTH <tim@tjth.co>
2019-08-06 18:45:40 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# k8s-dashboard
-Kubernetes Dashboard Setup with Basic Auth (Nginx Proxy)
+# k8s-dashboard-nginx
+
+Installation script for a Kubernetes Dashboard on a local cluster (The installation script assumes that kubectl has full-admin access to the Kubernetes Cluster).
+
+*NOTE: This setup should not be used in production, it allows unauthenticated full admin access to your Kubernetes Dashboard and is for Demo purposes only*
+
+## Nginx Dashboard with Basic Auth
+
+To complete the Dashboard installation, run the installation script from this repository. This script will communicate with your Kubernetes cluster to generate the required pods and services, before updating the docker-compose file with your user Token for the Kubernetes Dashboard
+
+```
+python install-dashboard.py
+```
+
+## Starting the Dashboard
+
+To start the Dashboard, simply "Up" the docker containers on your host - The Nginx proxy will then be available at http://[your-host]:8080
+
+```
+sudo docker-compose up -d
+```
---
+++ install-dashboard.py
@@ -0,0 +1,59 @@
+# Import modules
+import wget
+import subprocess
+import fileinput
+
+## Welcome Message
+print("##### Welcome to the TJTH Kubernetes Dashboard installer #####")
+
+## Get Dashboard IP/Hostname
+masterIp=raw_input("Please enter your K8s Master IP or Hostname: ")
+
+## Download Kubernetes Dashboard file
+dashYaml="/tmp/kubernetes-dashboard.yaml"
+dashYamlURL="https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml"
+downloadDash= wget.download(dashYamlURL, dashYaml)
+print("\nDownloading Kubernetes Dashboard Yaml File..")
+downloadDash
+
+## Set NodePort
+print("\nSetting Service to NodePort..")
+f=open(dashYaml,"a")
+f.write("  type: NodePort")
+f.close()
+
+## Create Dashboard in Kubernetes
+def createFromYaml(yamlFile):
+    print("\nDeploying: "+yamlFile)
+    subprocess.call(["kubectl","apply","-f",yamlFile])
+    print("\nDeployed..")
+createFromYaml(dashYaml)
+
+## Get Dashbaord Token
+def getToken():
+    print("\nGetting Token from Kube System..")
+    getTokenCmd="kubectl -n kube-system describe secrets $(kubectl -n kube-system get secret | grep 'dashboard-token' | awk {'print $1'}) | grep 'token:' | awk {'print $2'}"
+    getToken= subprocess.Popen(getTokenCmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    tokenName= getToken.communicate()[0]
+    return tokenName.rstrip()
+token = getToken()
+
+## Set Dashboard user as Admin
+def makeAdmin():
+    print("\nMaking Dashboard User a K8s Admin")
+    makeAdminCmd="kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard"
+    makeAdmin=subprocess.Popen(makeAdminCmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    confAdmin=makeAdmin.communicate()[0]
+makeAdmin()
+
+## Update docker-compose with Token
+nginxConf="config/nginx.conf"
+def updateNginxConf():
+    print("Updating Nginx Config with Token Env Variable and K8s Master Address..")
+    for line in fileinput.input(nginxConf,inplace=True):
+        print line.replace("CHANGETOKEN",token),
+    for line in fileinput.input(nginxConf,inplace=True):
+        print line.replace("CHANGEIP",masterIp),
+updateNginxConf()
+
+print("##### Task Complete! #####")
